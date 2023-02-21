5ffdd0bb2410d1377949f1709646d490c7dda081
Harshiii-k18 <harshitakumawat53@gmail.com>
2023-02-21 10:50:20 +0530

    sixth

--- js/script.js
+++ js/script.js
@@ -1,26 +1,20 @@
-let searchForm=document.querySelector('.search-form');
 
-document.querySelector('#search-btn').onclick=() =>{
-    searchForm.classList.toggle('active');
-    contact.classList.remove('active');
-    navbar.classList.remove('active');
-}
 let contact=document.querySelector('.contact');
 
 document.querySelector('#whatsapp-btn').onclick=() =>{
     contact.classList.toggle('active');
-    searchForm.classList.remove('active');
+   
     navbar.classList.remove('active');
 }
 let navbar=document.querySelector('.navbar');
 
 document.querySelector('#menu-btn').onclick=() =>{
     navbar.classList.toggle('active');
-    searchForm.classList.remove('active');
+    
     contact.classList.remove('active');
 }
 window.onscroll = () =>{
-    searchForm.classList.remove('active');
+   
     contact.classList.remove('active');
     navbar.classList.remove('active');
 }
