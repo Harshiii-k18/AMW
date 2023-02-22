57d432a6c9378fc36d365008712feb3cd4f5ea70
Harshiii-k18 <harshitakumawat53@gmail.com>
2023-02-23 02:24:58 +0530

    Seventh

--- js/script.js
+++ js/script.js
@@ -1,7 +1,74 @@
+function gotowhatsapp() {
 
+   
+   
+
+    var url = "https://wa.me/919636757008?text="
+     
+     
+
+    window.open(url, '_blank').focus();
+};
+function sendEmail(){
+    let name=document.getElementById("name").value;
+    let email=document.getElementById("email").value;
+    let number=document.getElementById("number").value;
+    let message=document.getElementById("message").value;
+    let body="Name:" + name + "<br> Email: " + email + "<br> Contact Number: " + number + 
+    "<br> Query: " + message;
+    
+    Email.send({
+        Host : "smtp.elasticemail.com",
+        Username : "antiquemarblekishangarh@gmail.com",
+        Password : "89B5301E9BC11846B9CE8DB0F070F6C1B5CA",
+        To : 'antiquemarbleworld@gmail.com',
+        From : "antiquemarblekishangarh@gmail.com",
+        Subject : "New Enquiry!",
+        Body : body
+    }).then(
+      message => alert(message)
+    );
+
+};
+function sendEmailForm(){
+    let name=document.getElementById("formname").value;
+    let email=document.getElementById("formemail").value;
+    let number=document.getElementById("contactnumber").value;
+    let message=document.getElementById("formmessage").value;
+    let body="Name:" + name + "<br> Email: " + email + "<br> Contact Number: " + number + 
+    "<br> Query: " + message;
+    
+    Email.send({
+        Host : "smtp.elasticemail.com",
+        Username : "antiquemarblekishangarh@gmail.com",
+        Password : "89B5301E9BC11846B9CE8DB0F070F6C1B5CA",
+        To : 'antiquemarbleworld@gmail.com',
+        From : "antiquemarblekishangarh@gmail.com",
+        Subject : "New Enquiry!",
+        Body : body
+    }).then(
+      message => alert(message)
+    );
+
+};
+function myFunction() {
+    var dots = document.getElementById("dots");
+    var moreText = document.getElementById("more");
+    var btnText = document.getElementById("myBtn");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Read more"; 
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Read less"; 
+      moreText.style.display = "inline";
+    }
+  };
 let contact=document.querySelector('.contact');
 
-document.querySelector('#whatsapp-btn').onclick=() =>{
+document.querySelector('#chat').onclick=() =>{
     contact.classList.toggle('active');
    
     navbar.classList.remove('active');
@@ -49,7 +116,7 @@ var swiper = new Swiper(".review-slider"  ,{
     loop:true,
     spaceBetween:20,
     autoplay:{
-        delay:7500,
+        delay:5000,
         disableOnInteraction:false,
 
     },
